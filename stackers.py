dced905666a52e6daebdcc24cd25f7b4d42f49bb
Lemursauce <lacarbo05@gmail.com>
2018-07-25 15:34:31 -0500

    basic stacking game

--- stackers.py
+++ stackers.py
@@ -18,22 +18,48 @@ class stack():
 		x = 0
 		y = 7
 		v = 1
-		while self.gaming and y >= 0:
 
+		cont = True
+		
+		blocks = [None]*8
+		while self.gaming and y >= 0 and cont == True:
+			
 			sense.set_pixel((x-v)%8,y, (0,0,0) )
 			sense.set_pixel(x,y, (0,0,255) )
 			time.sleep(.1)
-			sense.set_pixel(x,y, (0,0,255) )
+			sense.set_pixel(x,y, (0,0,0) )
 
 			for event in pygame.event.get():
 				if event.type == KEYDOWN:
-					sense.set_pixel(x,y, (0,0,255) )
-					y -= 1
+					blocks[y] = x
+					if y >= 7:
+						sense.set_pixel(x,y, (0,0,255) )
+						y -= 1
+					else:
+						if not blocks[y] == blocks[y+1]:
+							v = 0
+							cont = False
+						else:
+							sense.set_pixel(x,y, (0,0,255) )
+							y -= 1
 			
 			x = (x+v)%8
 			
 			if (x == 0) or (x == 7):
 				v *= -1
+		if cont == False:
+			for i in range(5):
+				sense.set_pixel(x,y, (0,0,255) )
+				time.sleep(.25)
+				sense.set_pixel(x,y, (0,0,0) )
+				time.sleep(.25)
+		else:
+			for i in range(5):
+				for j in range(len(blocks)):
+					sense.set_pixel(blocks[j],j, (0,0,255) )
+				time.sleep(.25)
+				sense.clear()
+				time.sleep(.25)
 
 if __name__ == "__main__":
 	try:
@@ -41,5 +67,4 @@ if __name__ == "__main__":
 		game.startGame()
 	except KeyboardInterrupt:
 		sense.clear()
-time.sleep(1)
 sense.clear()
